1c948987babd7aceb0113a1e0a3d46c350812c6d
Queenjiri <ajirijohnsonblessing@gmail.com>
2023-04-08 13:45:52 +0100

    This is a classic website

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+// import React from 'react';
+import Navbar  from './Navbar';
+
+function App() {
+  return (
+    <div className="showcase">
+      <div className="content">
+
+      <h1>Beautyplace</h1>
+      </div>
+
+
+      <hr></hr>
+     <Navbar/>
+     
+    </div>
+    
+  );
+  
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+
+
+const Navbar=()=>{
+  return(
+    <div className="content">
+   
+ <div className="links">
+   
+         <a href="/">Home</a>
+         <a href="/">Features</a>
+         <a href="/">Payment</a>
+         <a href="/">Schedule</a>
+
+        </div>
+
+        {/* <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div> */}
+
+    </div>
+  )
+}
+
+
+
+
+export default Navbar;
